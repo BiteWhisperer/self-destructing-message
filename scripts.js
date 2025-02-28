a762c29ae070ca3b828ef9ad2d9215bd2b0ee3b7
Dan <99664415+DanyBoy1384@users.noreply.github.com>
2025-02-28 22:39:13 +0330

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -20,6 +20,24 @@ function generateMessageId() {
     return 'msg-' + Math.random().toString(36).substr(2, 9);
 }
 
+function startCountdown(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            alert('This message will now self-destruct!');
+            window.location.href = window.location.pathname; // Clear URL params
+        }
+    }, 1000);
+}
+
 window.addEventListener('load', function() {
     const urlParams = new URLSearchParams(window.location.search);
     const messageId = urlParams.get('messageId');
@@ -34,17 +52,16 @@ window.addEventListener('load', function() {
                 <div id="messageContainer" class="container" style="display: none;">
                     <h1>Self-Destructing Message</h1>
                     <p id="messageContent">${message}</p>
-                    <p id="timerMessage">You only have 2 minutes to read this message.</p>
-                </div>`;
+                </div>
+                <div id="countdown">02:00</div>`;
             
             document.getElementById('okButton').addEventListener('click', function() {
                 document.getElementById('warningBox').style.display = 'none';
                 document.getElementById('messageContainer').style.display = 'block';
 
-                setTimeout(function() {
-                    alert('This message will now self-destruct!');
-                    window.location.href = window.location.pathname; // Clear URL params
-                }, 120000); // 120000 milliseconds = 2 minutes
+                const twoMinutes = 60 * 2,
+                      display = document.getElementById('countdown');
+                startCountdown(twoMinutes, display);
             });
 
             localStorage.removeItem(messageId);
